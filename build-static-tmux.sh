1fbc8cea9e3cf4a26892aad8e31d1b63f5b880c8
Miguel Saldivar <miguel.saldivar@hpe.com>
2024-12-10 07:48:01 -0800

    Version 3.5a

--- build-static-tmux.sh
+++ build-static-tmux.sh
@@ -15,7 +15,7 @@ COLOR_END="\033[0m"
 PGM="${0##*/}" # Program basename
 
 # Scriptversion
-VERSION=3.5
+VERSION=3.5a
 
 # How many lines of the error log should be displayed
 LOG_LINES=50
@@ -42,7 +42,7 @@ TMUX_BIN="tmux.${OS}-${ARCH}"
 ######################################
 ###### BEGIN VERSION DEFINITION ######
 ######################################
-TMUX_VERSION=3.5
+TMUX_VERSION=3.5a
 MUSL_VERSION=1.2.5
 NCURSES_VERSION=6.5
 LIBEVENT_VERSION=2.1.12
